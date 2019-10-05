80964baf1fb352ba02b40edf9974777acc595a05
satoshi <inu800ss@gmail.com>
2019-10-05 08:00:29 +0000

    rails04課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: {maximum: 15 }
+    validates :email, presence: true, format: {with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ }
+    validates :password, presence: true, length: {maxmum: 8,maximum: 32}, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+                         
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
-
-
-
 Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/help'
