b23d177372f8b19d2061ae3e56e3b40c641c2039
satoshi <inu800ss@gmail.com>
2019-11-03 10:39:43 +0000

    1st

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(session_params)
-    if user && user.authenticate(session_params)
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
